cd9a6249857b49502b1ed10834783b56dddefe9b
yohanse <mehabawyohanse793@gmail.com>
2024-10-19 11:18:49 +0300

    B+Tree: Add treeInsert function

--- RDB/btree.go
+++ RDB/btree.go
@@ -172,3 +172,22 @@ func nodeReplaceKidN(tree *BTree, new BNode, old BNode, idx uint16, kids ...BNod
 	}
 	nodeAppendRange(new, old, idx+inc, idx+1, old.nkeys()-(idx+1))
 }
+
+func treeInsert(tree *BTree, node BNode, key []byte, val []byte) BNode {
+	new := BNode(make([]byte, 2*BTREE_PAGE_SIZE))
+
+	idx := nodeLookupLE(node, key)
+	switch node.btype() {
+	case BNODE_LEAF:
+	if bytes.Equal(key, node.getKey(idx)) {
+		leafUpdate(new, node, idx, key, val)
+	} else {
+		leafInsert(new, node, idx+1, key, val)
+	}
+	case BNODE_NODE:
+		nodeInsert(tree, new, node, idx, key, val)
+	default:
+		panic("bad node!")
+	}
+	return new
+}
