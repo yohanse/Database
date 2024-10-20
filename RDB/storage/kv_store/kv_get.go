ec7456ff28a832e26b059f094217b844276fec96
yohanse <mehabawyohanse793@gmail.com>
2024-10-20 22:18:30 +0300

    add comments

--- RDB/storage/kv_store/kv_get.go
+++ RDB/storage/kv_store/kv_get.go
@@ -4,6 +4,10 @@ import (
 	"rdb/storage/b_plus_tree"
 )
 
+
+// func (db *KV) Get(key []byte) ([]byte, bool)
+//         This function retrieves the value associated with a given key from the B+ tree structure.
+
 func (db *KV) Get(key []byte) ([]byte, bool) {
     return db.tree.Get(key)
 }
--- RDB/storage/kv_store/kv_open.go
+++ RDB/storage/kv_store/kv_open.go
@@ -1,5 +1,9 @@
 package kvstore
 
+// func (db *KV) Open() error
+//     Usage: When a database application starts, it needs to connect to the database file (or create 
+//     it if it doesn't exist).
+
 func (db *KV) Open() error { // open or create
     db.tree.Get = db.pageRead   // read a page
     db.tree.New = db.pageAppend // apppend a page
