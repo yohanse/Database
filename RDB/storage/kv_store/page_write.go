7cdcba0ba3af34bad7ed9c05bf390e010d362a46
yohanse <mehabawyohanse793@gmail.com>
2024-10-20 22:58:17 +0300

    Add comment on page_write functionality

--- RDB/storage/kv_store/page_write.go
+++ RDB/storage/kv_store/page_write.go
@@ -1,13 +1,22 @@
 package kvstore
 
+import (
+	"golang.org/x/sys/unix"
+	"rdb/storage/b_plus_tree"
+)
+
+// writePages writes the in-memory pages to the file, extending the mmap if needed and updating the 
+// flushed page count.
+
+
 func writePages(db *KV) error {
     // extend the mmap if needed
-    size := (int(db.page.flushed) + len(db.page.temp)) * BTREE_PAGE_SIZE
+    size := (int(db.page.flushed) + len(db.page.temp)) * b_plus_tree.BTREE_PAGE_SIZE
     if err := extendMmap(db, size); err != nil {
         return err
     }
     // write data pages to the file
-    offset := int64(db.page.flushed * BTREE_PAGE_SIZE)
+    offset := int64(db.page.flushed * b_plus_tree.BTREE_PAGE_SIZE)
     if _, err := unix.Pwritev(db.fd, db.page.temp, offset); err != nil {
         return err
     }
