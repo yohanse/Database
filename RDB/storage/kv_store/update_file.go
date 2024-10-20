9d44ba24be1c65f65a8bbdd5a2c5873948a8ae32
yohanse <mehabawyohanse793@gmail.com>
2024-10-20 22:29:35 +0300

    add update file functionality

---
+++ RDB/storage/kv_store/update_file.go
@@ -0,0 +1,16 @@
+func updateFile(db *KV) error {
+    // 1. Write new nodes.
+    if err := writePages(db); err != nil {
+        return err
+    }
+    // 2. `fsync` to enforce the order between 1 and 3.
+    if err := syscall.Fsync(db.fd); err != nil {
+        return err
+    }
+    // 3. Update the root pointer atomically.
+    if err := updateRoot(db); err != nil {
+        return err
+    }
+    // 4. `fsync` to make everything persistent.
+    return syscall.Fsync(db.fd)
+}
