ad9d4921f27cca646c7c7c79d843aa71626ef397
yohanse <mehabawyohanse793@gmail.com>
2024-10-20 22:28:22 +0300

    Add comment on the del functionality

--- RDB/storage/kv_store/kv_del.go
+++ RDB/storage/kv_store/kv_del.go
@@ -1,5 +1,7 @@
 package kvstore
 
+// he Del method in your KV struct is intended to delete a key-value pair from the B+ tree and 
+// then update the database file to reflect this change. 
 func (db *KV) Del(key []byte) (bool, error) {
     deleted, _ := db.tree.Delete(key)
     return deleted, updateFile(db)
