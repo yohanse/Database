99ecf5bc698684e75eb6eef86b47ad86e4d722f6
yohanse <mehabawyohanse793@gmail.com>
2024-10-20 22:17:29 +0300

    add comment for page read.

--- RDB/storage/kv_store/page_read.go
+++ RDB/storage/kv_store/page_read.go
@@ -4,6 +4,9 @@ import (
 	"rdb/storage/b_plus_tree"
 )
 
+// func pageRead(p *page, n uint64) ([]byte, error)
+//     The pageRead function retrieves a page of data from the memory-mapped chunks based on a page 
+//     number pointer
 func (db *KV) pageRead(ptr uint64) []byte {
     start := uint64(0)
     for _, chunk := range db.mmap.chunks {
