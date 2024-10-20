614be988f5c085904cb47b6a8c32ecb8c5d2444c
yohanse <mehabawyohanse793@gmail.com>
2024-10-20 23:34:25 +0300

    add comment on update or revert

--- RDB/storage/kv_store/update_or_revert_file.go
+++ RDB/storage/kv_store/update_or_revert_file.go
@@ -1,5 +1,9 @@
 package kvstore
 
+// updateOrRevert attempts to update the database file. 
+// If the update fails, it reverts the in-memory state using the provided metadata 
+// and clears temporary data. Returns an error if the update operation fails.
+
 func updateOrRevert(db *KV, meta []byte) error {
     // 2-phase update
     err := updateFile(db)
