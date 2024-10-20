91be21708fcfa832e8cfc4fbd49e6565617e2d55
yohanse <mehabawyohanse793@gmail.com>
2024-10-20 23:10:55 +0300

    add comment on update root functionality

--- RDB/storage/kv_store/update_root.go
+++ RDB/storage/kv_store/update_root.go
@@ -5,6 +5,9 @@ import (
 	"syscall"
 )
 
+// updateRoot writes the current metadata of the database to the meta page at offset 0.
+// It returns an error if the write operation fails.
+
 func updateRoot(db *KV) error {
     if _, err := syscall.Pwrite(db.fd, saveMeta(db), 0); err != nil {
         return fmt.Errorf("write meta page: %w", err)
