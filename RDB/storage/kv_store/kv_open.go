4f5b3e620138b03e79eda460f4a11de60a688296
yohanse <mehabawyohanse793@gmail.com>
2024-10-20 22:51:09 +0300

    add comment on pageAppend functionality

--- RDB/storage/kv_store/kv_open.go
+++ RDB/storage/kv_store/kv_open.go
@@ -8,4 +8,5 @@ func (db *KV) Open() error { // open or create
     db.tree.Get = db.pageRead   // read a page
     db.tree.New = db.pageAppend // apppend a page
     db.tree.Del = func(uint64) {}
+	return nil
 }
--- RDB/storage/kv_store/page_append.go
+++ RDB/storage/kv_store/page_append.go
@@ -1,5 +1,7 @@
 package kvstore
 
+// pageAppend adds a new page (node) to the in-memory storage and returns its index pointer.
+
 func (db *KV) pageAppend(node []byte) uint64 {
     ptr := db.page.flushed + uint64(len(db.page.temp)) // just append
     db.page.temp = append(db.page.temp, node)
