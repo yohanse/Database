9a4bb397f3db7e7cdf9c223227bdff0585e2c75e
yohanse <mehabawyohanse793@gmail.com>
2024-10-20 22:32:31 +0300

    add create file functionality

---
+++ RDB/storage/kv_store/create_file.go
@@ -0,0 +1,23 @@
+package kvstore
+
+func createFileSync(file string) (int, error) {
+    // obtain the directory fd
+    flags := os.O_RDONLY | syscall.O_DIRECTORY
+    dirfd, err := syscall.Open(path.Dir(file), flags, 0o644)
+    if err != nil {
+        return -1, fmt.Errorf("open directory: %w", err)
+    }
+    defer syscall.Close(dirfd)
+    // open or create the file
+    flags = os.O_RDWR | os.O_CREATE
+    fd, err := syscall.Openat(dirfd, path.Base(file), flags, 0o644)
+    if err != nil {
+        return -1, fmt.Errorf("open file: %w", err)
+    }
+    // fsync the directory
+    if err = syscall.Fsync(dirfd); err != nil {
+        _ = syscall.Close(fd)  // may leave an empty file
+        return -1, fmt.Errorf("fsync directory: %w", err)
+    }
+    return fd, nil
+}
