5ca260dd941bacffe7f46b5da01bf8d8d93498ab
yohanse <mehabawyohanse793@gmail.com>
2024-10-21 18:05:37 +0300

    add freelist struct

---
+++ RDB/storage/linked_list/freelist.go
@@ -0,0 +1,15 @@
+package linkedlist
+
+type FreeList struct {
+    // callbacks for managing on-disk pages
+    get func(uint64) []byte // read a page
+    new func([]byte) uint64 // append a new page
+    set func(uint64) []byte // update an existing page
+    // persisted data in the meta page
+    headPage uint64 // pointer to the list head node
+    headSeq  uint64 // monotonic sequence number to index into the list head
+    tailPage uint64
+    tailSeq  uint64
+    // in-memory states
+    maxSeq uint64 // saved `tailSeq` to prevent consuming newly added items
+}
