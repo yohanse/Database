51483874ec2f49e58d52686d77eb551803a19882
yohanse <mehabawyohanse793@gmail.com>
2024-12-13 23:44:24 +0300

    fix load meta page by adding assert condition

--- RDB/storage/kv_store/load_meta.go
+++ RDB/storage/kv_store/load_meta.go
@@ -5,6 +5,14 @@ import (
 )
 
 func loadMeta(db *KV, data []byte) {
-	db.tree.Root = binary.LittleEndian.Uint64(data[16:])
-	db.page.flushed = binary.LittleEndian.Uint64(data[24:])
+    // Read the signature
+    if string(data[:16]) != DB_SIG {
+        panic("Invalid database signature")
+    }
+
+    // Read the root pointer
+    db.tree.Root = binary.LittleEndian.Uint64(data[16:24])
+
+    // Read the page usage count
+    db.page.flushed = binary.LittleEndian.Uint64(data[24:])
 }
